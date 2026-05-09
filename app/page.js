6ab0a0b4a8de65de0df0deed85109086922c05fd
Purevdash <purewdashtumee@gmail.com>
2026-05-09 19:50:10 +0800

    redirect to login

--- app/page.js
+++ app/page.js
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-gray-950 text-white flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Хэвлэлийн тооцоолуур</h1>
-    </main>
-  );
+  redirect('/login')
 }
